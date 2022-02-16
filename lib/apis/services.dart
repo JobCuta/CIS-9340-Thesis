a700c0b80b6411d897fbea03f61879ccbf3ad552
JobCuta <hanrhee1@gmail.com>
2022-02-16 13:54:46 +0800

    updated services and forms

--- lib/apis/services.dart
+++ lib/apis/services.dart
@@ -1,25 +1,29 @@
+import 'package:flutter_application_1/constants/forms.dart';
 import 'package:get/get.dart';
 
 class UserProvider extends GetConnect {
-  static const domain = '';
+
+  static const domain = "https://kasiyanna-efd6n.ondigitalocean.app/";
 
   final Map paths = {
-    "login": "",
-    "register": "",
-    "emailCode": "",
-    "forgot": ""
+    "login": "api/v1/auth/registration/",
+    "logout":"api/v1/auth/logout/",
+    "register": "api/v1/auth/login/",
+    "forgot": "",
+    "getUser": "api/v1/auth/user/",
   };
 
-  Future<Response> login(Map loginData) => post(
-        domain + paths["login"],
-        loginData,
-      );
+  //POST
+  Future<Response> login(LoginForm loginData) => post( domain + paths["login"], loginData);
 
-  Future<Response> register(Map userData) =>
-      post(domain + paths["register"], userData);
+  Future<Response> register(Map userData) => post( domain + paths["register"], userData);
 
-  Future<Response> emailCode() => get(domain + paths["emailCode"]);
+  Future<Response> forgotPassword(String email) => post(domain + paths["forgot"], email);
 
-  Future<Response> forgotPassword(String email) =>
-      get(domain + paths["forgot"]);
+  //GET
+  Future<Response> logout() => get(domain + paths["logout"]);
+  
+  //PUT
+  Future<Response> updateUser(UserForm userData) => put(domain + paths["getUser"], userData);
+
 }
--- lib/constants/forms.dart
+++ lib/constants/forms.dart
@@ -1,4 +1,38 @@
 class LoginForm {
-  late String user;
-  late String login;
+  late String email;
+  late String pass;
+
+  Map<String, String> form() {
+    return {"email": email, "password": pass};
+  }
+}
+
+class RegisterForm {
+  late String email;
+  late String pass1;
+  late String pass2;
+
+  Map<String, String> form() {
+    return {"email": email, "password1": pass1, "password2": pass2};
+  }
+}
+
+class UserForm {
+  late int id;
+  late String email;
+  late String fname;
+  late String lname;
+  late String bday;
+  late String gender;
+
+  Map<String, dynamic> form() {
+    return {
+      "id": id,
+      "email": email,
+      "first_name": fname,
+      "last_name": lname,
+      "date_of_birth": bday,
+      "gender": gender,
+    };
+  }
 }
