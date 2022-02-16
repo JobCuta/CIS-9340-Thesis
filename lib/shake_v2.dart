35978ce322b8ef5ef6dbd43382ca8118c1860f63
KARINA YSABELLE ALCID <2193079@slu.edu.ph>
2022-02-16 17:34:10 +0800

    added the countdown timer

--- lib/shake_v2.dart
+++ lib/shake_v2.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shake/shake.dart';
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'dart:math';
 import 'dart:async';
 
@@ -15,32 +16,45 @@ class shake extends StatefulWidget {
 }
 
 class _shakeState extends State<shake> {
+  bool _startExercise = false;
+
   // variables for the PageView.builder
-  PageController _pageController = PageController();
-  var random = new Random();
-  var currentPageValue = 0.0;
+  final PageController _pageController = PageController();
+  final CountDownController _timerController = CountDownController();
+  var random = Random();
+  var counter = 0;
+
   List<String> screenTitle = [
     'Shake your phone!',
     'Randomising...',
     'Take a minute and breathe and meditate',
-    'Listen to the relaxing sounds of rain',
-    'PLACEHOLDER 3rd EXERCISE'
+    'Take a minute and breathe and meditate',
+    "Pay attention to an object's movement",
+    "Pay attention to an object's movement",
+    'Go for a short, mindful walk',
+    'Go for a short, mindful walk'
   ];
 
   List<String> screenDescription = [
     'Randomize the first exercise you will do!',
     "Let’s see what you’ll get!",
-    '',
-    '',
-    ''
+    'Get comfortable and focus on your breathing',
+    'Get comfortable and focus on your breathing',
+    'Keep your attention on the present',
+    'Keep your attention on the present',
+    '"With each step, a gentle wind blows"',
+    '"With each step, a gentle wind blows"'
   ];
 
   List<String> screenImages = [
     'assets/images/phone.png',
     'assets/images/phone.png',
-    'assets/images/sample.png',
-    'assets/images/sample.png',
-    'assets/images/sample.png'
+    'assets/images/meditating.png',
+    'assets/images/meditating.png',
+    'assets/images/breathing.png',
+    'assets/images/breathing.png',
+    'assets/images/walking.png',
+    'assets/images/walking.png'
   ];
 
   // random colors for now
@@ -48,7 +62,10 @@ class _shakeState extends State<shake> {
     Colors.blue[300],
     Colors.red[500],
     Colors.yellow[700],
+    Colors.yellow[700],
     Colors.purple[900],
+    Colors.purple[900],
+    Colors.green[800],
     Colors.green[800]
   ];
 
@@ -72,10 +89,12 @@ class _shakeState extends State<shake> {
   }
 
   startTime() async {
-    // random number 1 to 3, changes page after 5 seconds
-    int randomExercise = random.nextInt(3) + 2;
+    var randomExercises = [2, 4, 6];
+    var randomExercise = (randomExercises..shuffle()).first;
     var duration = const Duration(seconds: 5);
     return Timer(duration, () {
+      counter = randomExercise;
+      print('counter: ' + counter.toString());
       _pageController.jumpToPage(randomExercise);
       // duration: const Duration(milliseconds: 500), curve: Curves.easeOut);
     });
@@ -83,6 +102,9 @@ class _shakeState extends State<shake> {
 
   @override
   Widget build(BuildContext context) {
+    var _initialCountdown = false;
+    // var _duration = 10;
+
     return MaterialApp(
         home: Scaffold(
             body: PageView.builder(
@@ -94,8 +116,68 @@ class _shakeState extends State<shake> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Visibility(
+                visible: position == 2 || position == 4 || position == 6,
+                child: Column(
+                  children: [
+                    const Text('Relax and get ready...'),
+                    CircularCountDownTimer(
+                        duration: 10,
+                        initialDuration: 0,
+                        controller: _timerController,
+                        width: MediaQuery.of(context).size.width / 2.5,
+                        height: MediaQuery.of(context).size.height / 2.5,
+                        ringColor: Colors.orange[300]!,
+                        fillColor: Colors.orange[100]!,
+                        backgroundColor: Colors.orange[500],
+                        backgroundGradient: null,
+                        strokeWidth: 20.0,
+                        strokeCap: StrokeCap.round,
+                        textStyle: const TextStyle(
+                            fontSize: 33.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+
+                        // Format for the Countdown Text.
+                        textFormat: CountdownTextFormat.MM_SS,
+                        isReverse: true,
+                        isReverseAnimation: false,
+                        isTimerTextShown: true,
+                        autoStart: true,
+                        onComplete: () {
+                          _pageController.jumpToPage(position + 1);
+                        }),
+                  ],
+                ),
+              ),
+              Visibility(
+                visible: position == 3 || position == 5 || position == 7,
+                child: CircularCountDownTimer(
+                    duration: 60,
+                    initialDuration: 0,
+                    controller: _timerController,
+                    width: MediaQuery.of(context).size.width / 2.5,
+                    height: MediaQuery.of(context).size.height / 2.5,
+                    ringColor: Colors.orange[300]!,
+                    fillColor: Colors.orange[100]!,
+                    backgroundColor: Colors.orange[500],
+                    strokeWidth: 20.0,
+                    strokeCap: StrokeCap.round,
+                    textStyle: const TextStyle(
+                        fontSize: 33.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+
+                    // Format for the Countdown Text.
+                    textFormat: CountdownTextFormat.MM_SS,
+                    isReverse: true,
+                    isReverseAnimation: false,
+                    isTimerTextShown: true,
+                    autoStart: true,
+                    onComplete: () {}),
+              ),
               CircleAvatar(
-                radius: 60,
+                radius: 100,
                 backgroundColor: colors[position],
                 child: Image(
                     image: AssetImage(screenImages[position]),
