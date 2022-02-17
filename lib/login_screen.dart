55bde6271d875aea41387a50f019632b361a1395
Patrick Dalasen <2190540@slu.edu.ph>
2022-02-17 10:07:36 +0800

    added input variables, issues fixed

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -45,11 +45,14 @@ class LoginWidgets extends StatefulWidget {
 }
 
 class _LoginWidgetsState extends State<LoginWidgets> {
+
+  String email = '';
+  String password = '';
   bool _isObscure = true;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
         children: <Widget> [
           const Padding(
             padding: EdgeInsets.all(24.0),
@@ -77,92 +80,101 @@ class _LoginWidgetsState extends State<LoginWidgets> {
             ),
           ),
           Padding(padding: const EdgeInsets.only(left: 20.0, right: 20),
-            child: Column(
-              children: <Widget> [
-                Row(
-                  children: const [
-                    Text('Email',
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.w700,
-                        color: Color.fromRGBO(94, 102, 104, 1)
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 5.0,),
-                TextField(
-                  style: const TextStyle(
-                    fontSize: 14.0,
-                  ),
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    hintText: 'Enter your email',
-                    hintStyle: TextStyle(
-                      fontWeight: FontWeight.w400,
-                      color: Colors.grey[700],
-                    ),
-                    contentPadding: const EdgeInsets.symmetric(vertical: 13.0, horizontal: 14.0),
-                  ),
-                ),
-                const SizedBox(height: 20.0,),
-                Row(
-                  children: const [
-                    Text('Password',
-                      style: TextStyle(
+            child: Form(
+              child: Column(
+                children: <Widget> [
+                  Row(
+                    children: const [
+                      Text('Email',
+                        style: TextStyle(
                           fontSize: 12,
                           fontWeight: FontWeight.w700,
                           color: Color.fromRGBO(94, 102, 104, 1)
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 5.0,),
-                TextField(
-                  obscureText: _isObscure,
-                  style: const TextStyle(
-                    fontSize: 14.0,
-                  ),
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    hintText: 'Enter your password',
-                    hintStyle: TextStyle(
-                      fontWeight: FontWeight.w400,
-                      color: Colors.grey[700],
-                    ),
-                    contentPadding: const EdgeInsets.symmetric(vertical: 13.0, horizontal: 14.0),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        _isObscure ? Icons.visibility : Icons.visibility_off),
-                        onPressed: () {
-                          setState(() {
-                            _isObscure = !_isObscure;
-                          });
-                        },
-                      ),
-                    )
-                  ),
-                const SizedBox(height: 5.0,),
-                SizedBox(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          //Go to Forgot Your Password Screen
-                        },
-                        child: const Text('Forgot your password?',
-                          style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w600,
-                              color: Color.fromRGBO(94, 102, 104, 1)
-                          ),
                         ),
-                      )
+                      ),
                     ],
                   ),
-                ),
-              ],
+                  const SizedBox(height: 5.0,),
+                  TextField(
+                    style: const TextStyle(
+                      fontSize: 14.0,
+                    ),
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(),
+                      hintText: 'Enter your email',
+                      hintStyle: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        color: Colors.grey[700],
+                      ),
+                      contentPadding: const EdgeInsets.symmetric(vertical: 13.0, horizontal: 14.0),
+                    ),
+                    onChanged: (val) {
+                      setState(() => email = val);
+                    },
+                  ),
+                  const SizedBox(height: 20.0,),
+                  Row(
+                    children: const [
+                      Text('Password',
+                        style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w700,
+                            color: Color.fromRGBO(94, 102, 104, 1)
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 5.0,),
+                  TextField(
+                    obscureText: _isObscure,
+                    style: const TextStyle(
+                      fontSize: 14.0,
+                    ),
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(),
+                      hintText: 'Enter your password',
+                      hintStyle: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        color: Colors.grey[700],
+                      ),
+                      contentPadding: const EdgeInsets.symmetric(vertical: 13.0, horizontal: 14.0),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _isObscure ? Icons.visibility : Icons.visibility_off),
+                          onPressed: () {
+                            setState(() {
+                              _isObscure = !_isObscure;
+                            });
+                          },
+                        ),
+                      ),
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                    ),
+                  const SizedBox(height: 5.0,),
+                  SizedBox(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            //Go to Forgot Your Password Screen
+                          },
+                          child: const Text('Forgot your password?',
+                            style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w600,
+                                color: Color.fromRGBO(94, 102, 104, 1)
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 100.0,),
+                ],
+              ),
             ),
           ),
         ],
