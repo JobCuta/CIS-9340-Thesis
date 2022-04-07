40f130df743132ca3d77ca2ee234d9264e863c68
KARINA YSABELLE ALCID <2193079@slu.edu.ph>
2022-04-07 08:12:27 +0800

    achievements page ui fixes

--- lib/screens/SideMenu/AchievementsScreen.dart
+++ lib/screens/SideMenu/AchievementsScreen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/constants/colors.dart';
+import 'package:flutter_application_1/controllers/levelController.dart';
+import 'package:flutter_application_1/screens/main/SideMenu.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class AchievementsScreen extends StatefulWidget {
   const AchievementsScreen({Key? key}) : super(key: key);
@@ -8,70 +14,141 @@ class AchievementsScreen extends StatefulWidget {
 }
 
 class _AchievementsScreenState extends State<AchievementsScreen> {
+  final LevelController _levelController = Get.put(LevelController());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+          title: const Text('Achievements'),
+          primary: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0),
+      drawer: const SideMenu(),
       body: Stack(
         children: [
           Container(
               decoration: const BoxDecoration(
                   image: DecorationImage(
-                      image: AssetImage('assets/background_images/achievements_background.png'),
+                      image: AssetImage(
+                          'assets/background_images/orange_background.png'),
                       fit: BoxFit.cover))),
           SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(8, 36, 8, 10),
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(15, 50, 15, 10),
               child: Column(
                 children: [
                   const SizedBox(height: 36),
                   const Image(
-                    image: AssetImage('assets/images/user_avatar.png'), width: 328, height: 148,
+                    image: AssetImage('assets/images/user_avatar.png'),
+                    width: 328,
+                    height: 148,
                   ),
                   const SizedBox(height: 10),
-                  const Divider(height: 3.0, color: Colors.white),
-                  const SizedBox(height: 10),
-                  const Text('Achievements', style: TextStyle(fontWeight: FontWeight.w600, fontSize: 24.0, color: Color.fromRGBO(237, 172, 5, 1))),
-                  const SizedBox(height: 10),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 5.0),
+                    child: Divider(
+                      indent: 10,
+                      endIndent: 10,
+                      height: 5.0,
+                      color: Colors.white,
+                      thickness: 1.5,
+                    ),
+                  ),
                   Container(
-                    height: 132, width: MediaQuery.of(context).size.width - 32,
+                    margin: const EdgeInsets.symmetric(vertical: 10),
+                    child: Text('Achievements',
+                        style: Theme.of(context).textTheme.subtitle1?.copyWith(
+                              fontWeight: FontWeight.w600,
+                              color:
+                                  Theme.of(context).colorScheme.neutralWhite01,
+                            )),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 10),
                     decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(10.0)),
                     child: Padding(
-                      padding: const EdgeInsets.only(left: 10, top: 16.0),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 15.0, horizontal: 15),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          const Text('Current Level:' + ' Level 1',
-                            style: TextStyle(
-                              color: Color.fromRGBO(255, 193, 34, 1),
-                              fontSize: 20.0, fontWeight: FontWeight.w600,
+                          Text(
+                              'Current Level: Level ${_levelController.currentLevel.value}',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .subtitle1
+                                  ?.copyWith(
+                                    fontWeight: FontWeight.w600,
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .sunflowerYellow01,
+                                  )),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 15),
+                            child: LinearPercentIndicator(
+                              barRadius: const Radius.circular(24),
+                              curve: Curves.easeIn,
+                              lineHeight: 25,
+                              percent: _levelController.currentXp / 1000,
+                              progressColor: Theme.of(context)
+                                  .colorScheme
+                                  .sunflowerYellow01,
+                              backgroundColor: const Color(0xffFF9B26),
+                              animation: true,
+                              animationDuration: 200,
+                              animateFromLastPercent: true,
+                              addAutomaticKeepAlive: true,
                             ),
                           ),
-                          const SizedBox(height: 12),
-                          Container(
-                            width: MediaQuery.of(context).size.width - 82, height: 11,
-                            decoration: BoxDecoration(
-                                color: const Color.fromRGBO(255, 190, 24, 1),
-                                borderRadius: BorderRadius.circular(20)
-                            ),
-                          ),
-                          const SizedBox(height: 12),
-                          const Text('19000' + ' / ' + '20000' + ' to unlock next level',
-                            style: TextStyle(fontSize: 14.0),
+                          Text(
+                            '${_levelController.currentXp} / ${_levelController.xpForNextLevel} to unlock next level',
+                            style:
+                                Theme.of(context).textTheme.bodyText2?.copyWith(
+                                      fontWeight: FontWeight.w400,
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .neutralBlack02,
+                                    ),
                           ),
                           const SizedBox(height: 7),
                           Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Text('Continue Adventure', style: const TextStyle(fontSize: 16.0)),
-                              SizedBox(
-                                  width: MediaQuery.of(context).size.width - 216,
-                                  child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: const [
-                                        Text('Go', style: TextStyle(fontSize: 16.0, color: Color.fromRGBO(172, 178, 180, 1), fontWeight: FontWeight.bold),),
-                                        Icon(Icons.chevron_right, color: Color.fromRGBO(172, 178, 180, 1), size: 25,)
-                                      ])
+                              Text('Continue Adventure',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyText1
+                                      ?.copyWith(
+                                        fontWeight: FontWeight.w400,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .neutralBlack02,
+                                      )),
+                              RichText(
+                                text: TextSpan(children: [
+                                  TextSpan(
+                                      text: 'Go',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodyText1
+                                          ?.copyWith(
+                                              fontWeight: FontWeight.w700,
+                                              color: Theme.of(context)
+                                                  .colorScheme
+                                                  .neutralGray02)),
+                                  WidgetSpan(
+                                      alignment: PlaceholderAlignment.middle,
+                                      child: Icon(
+                                          Icons.keyboard_arrow_right_sharp,
+                                          size: 30,
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .neutralGray02))
+                                ]),
                               )
                             ],
                           )
@@ -79,36 +156,63 @@ class _AchievementsScreenState extends State<AchievementsScreen> {
                       ),
                     ),
                   ),
-                  const SizedBox(height: 10),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      achievementContainers('assets/achievements/apayao_adventure_achievements.png', 'General Achievements', '40', '60'),
-                      achievementContainers('assets/achievements/apayao_adventure_achievements.png', 'Apayao Adventure', '10', '10')
+                      achievementContainers(
+                          'assets/achievements/general_adventure_achievements.svg',
+                          'General Achievements',
+                          '40',
+                          '60'),
+                      achievementContainers(
+                          'assets/achievements/apayao_adventure_achievements.svg',
+                          'Apayao Adventure',
+                          '10',
+                          '10')
                     ],
                   ),
-                  const SizedBox(height: 10),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      achievementContainers('assets/achievements/kalinga_adventure_achievements.png', 'Kalinga Adventure', '5', '10'),
-                      achievementContainers('assets/achievements/abra_adventure_achievements.png', 'Abra Adventure', '7', '10'),
+                      achievementContainers(
+                          'assets/achievements/kalinga_adventure_achievements.svg',
+                          'Kalinga Adventure',
+                          '5',
+                          '10'),
+                      achievementContainers(
+                          'assets/achievements/abra_adventure_achievements.svg',
+                          'Abra Adventure',
+                          '7',
+                          '10'),
                     ],
                   ),
-                  const SizedBox(height: 10),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      achievementContainers('assets/achievements/abra_adventure_achievements.png', 'Mt. Province Adventure', '9', '10'),
-                      achievementContainers('assets/achievements/ifugao_adventure_achievements.png', 'Ifugao Adventure', '1', '10'),
+                      achievementContainers(
+                          'assets/achievements/mtprovince_adventure_achievements.svg',
+                          'Mt. Province Adventure',
+                          '9',
+                          '10'),
+                      achievementContainers(
+                          'assets/achievements/ifugao_adventure_achievements.svg',
+                          'Ifugao Adventure',
+                          '1',
+                          '10'),
                     ],
                   ),
-                  const SizedBox(height: 10),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      achievementContainers('assets/achievements/benguet_adventure_achievements.png', 'Benguet Adventure', '5', '10'),
-                      achievementContainers('assets/achievements/warrior_adventure_achievements.png', 'Mini Games Warrior', '3', '10'),
+                      achievementContainers(
+                          'assets/achievements/benguet_adventure_achievements.svg',
+                          'Benguet Adventure',
+                          '5',
+                          '10'),
+                      achievementContainers(
+                          'assets/achievements/warrior_adventure_achievements.svg',
+                          'Mini Games Warrior',
+                          '3',
+                          '10'),
                     ],
                   )
                 ],
@@ -120,36 +224,62 @@ class _AchievementsScreenState extends State<AchievementsScreen> {
     );
   }
 
-  achievementContainers(path, String title, String noOfCurrAchievements, String noOfTotalAchievements) {
-    return Container(
-      height: MediaQuery.of(context).size.height * 0.2509, width: MediaQuery.of(context).size.width * 0.4611 -5,
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20.0)
-      ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset (path, height: 60, width: 60),
-            const SizedBox(height: 6.0),
-            Text(title, style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 12.0)),
-            const SizedBox(height: 6.0),
-            expBar(),
-            const SizedBox(height: 6.0),
-            Text(noOfCurrAchievements + ' out of ' + noOfTotalAchievements)
-          ],
+  achievementContainers(path, String title, String noOfCurrAchievements,
+      String noOfTotalAchievements) {
+    return Expanded(
+      child: Container(
+        margin: const EdgeInsets.all(5),
+        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(20.0)),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Theme.of(context).colorScheme.neutralWhite01,
+                          Theme.of(context).colorScheme.sunflowerYellow04,
+                        ],
+                      )),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: SvgPicture.asset(path, width: 45, height: 45),
+                  )),
+              const SizedBox(height: 6.0),
+              Text(title,
+                  style: Theme.of(context).textTheme.caption?.copyWith(
+                      fontWeight: FontWeight.w600,
+                      color: Theme.of(context).colorScheme.neutralBlack02)),
+              const SizedBox(height: 6.0),
+              LinearPercentIndicator(
+                barRadius: const Radius.circular(24),
+                curve: Curves.easeIn,
+                lineHeight: 10,
+                percent: int.parse(noOfCurrAchievements) /
+                    int.parse(noOfTotalAchievements),
+                progressColor: Theme.of(context).colorScheme.intGreen05,
+                backgroundColor: const Color(0xffC4C4C4),
+                animation: true,
+                animationDuration: 1500,
+                animateFromLastPercent: true,
+                addAutomaticKeepAlive: true,
+              ),
+              const SizedBox(height: 6.0),
+              Text(noOfCurrAchievements + ' out of ' + noOfTotalAchievements,
+                  style: Theme.of(context).textTheme.caption?.copyWith(
+                      fontWeight: FontWeight.w400,
+                      color: Theme.of(context).colorScheme.neutralBlack02))
+            ],
+          ),
         ),
-      ),
-    );
-  }
-
-  expBar() {
-    return Container(
-      width: 110, height: 9,
-      decoration: BoxDecoration(
-          color: const Color.fromRGBO(76, 154, 42, 1),
-          borderRadius: BorderRadius.circular(20)
       ),
     );
   }
