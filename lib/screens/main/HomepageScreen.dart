785adec2886d28a4d5e89bb195c44cf26fc0d030
Lance <Lance-Awangan>
2022-04-17 18:47:03 +0800

    Added UserJourneyScreen

--- lib/screens/main/HomepageScreen.dart
+++ lib/screens/main/HomepageScreen.dart
@@ -9,6 +9,7 @@ import 'package:flutter_application_1/screens/MiniGames/Sudoku/SudokuScreen.dart
 import 'package:flutter_application_1/screens/main/AdventureHomeScreen.dart';
 import 'package:flutter_application_1/screens/main/CalendarScreen.dart';
 import 'package:flutter_application_1/screens/main/EntriesScreen.dart';
+import 'package:flutter_application_1/screens/main/UserJourney.dart';
 import 'package:flutter_application_1/widgets/LevelExperienceModal.dart';
 import 'package:flutter_application_1/widgets/LevelTasksTodayModal.dart';
 import 'package:flutter_application_1/constants/colors.dart';
@@ -38,6 +39,7 @@ class _HomePageScreenState extends State<HomePageScreen> {
     const CalendarScreen(),
     const HomePage(),
     const AdventureHomeScreen(),
+    const UserJourneyScreen(),
     const Text('Mini-games screen')
   ];
 
