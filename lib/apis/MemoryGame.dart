2019c8588557ad044b28bfd59e23f33632f118c1
Patrick Dalasen <2190540@slu.edu.ph>
2022-04-27 21:26:55 +0800

    added memory game to adventure mode

---
+++ lib/apis/MemoryGame.dart
@@ -0,0 +1,13 @@
+import 'package:hive/hive.dart';
+
+part 'MemoryGame.g.dart';
+
+@HiveType(typeId: 13)
+class MemoryGame extends HiveObject{
+  @HiveField(0)
+  List<bool> provinceCompleted;
+
+  MemoryGame({
+    required this.provinceCompleted
+  });
+}
