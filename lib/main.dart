2c9cad81626e91326313b9d276b63bef1d2d3ecb
Patrick Dalasen <2190540@slu.edu.ph>
2022-04-28 18:45:48 +0800

    fixed error in memory adventure

--- lib/main.dart
+++ lib/main.dart
@@ -143,7 +143,7 @@ class _MainState extends State<Main> {
   Widget build(BuildContext context) {
     return GetMaterialApp(
         title: 'Kasiyanna App',
-        initialRoute: '/homepage',
+        initialRoute: '/',
         getPages: [
           // intro
           GetPage(name: '/introScreen', page: () => const IntroductionScreen()),
